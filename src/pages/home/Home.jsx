fd780d541066418049cb196549df1b338d6df639
Mahier Sydow <mahier.sydow8@gmail.com>
2021-07-24 16:55:19 +0900

    first commit/navbar/header/homepage

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './home.css'
+import Header from '../../header/Header';
+
+function Home() {
+    return (
+        <div className="home">
+        <Header />
+        homepage
+        </div>
+    )
+}
+
+export default Home
